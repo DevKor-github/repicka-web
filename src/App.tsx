c80a8df1496b11e98aa2e2d626f8ef921ac19187
halionaz <samhsj@korea.ac.kr>
2025-05-30 01:28:03 +0900

    feat: login btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <div>리피카 짱</div>;
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+      <h1>리피카 짱</h1>
+      <a href={`${import.meta.env.VITE_API_URL}/oauth2/authorization/kakao`}>카카오톡 로그인</a>
+      <a href={`${import.meta.env.VITE_API_URL}/oauth2/authorization/google`}>구글 로그인</a>
+    </div>
+  );
 }
 
 export default App;
