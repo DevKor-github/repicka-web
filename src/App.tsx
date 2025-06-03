f0d51e5bed3bc2ed068a06c5cff2470e93c663f0
halionaz <samhsj@korea.ac.kr>
2025-06-03 16:37:14 +0900

    feat: Tanstack Query 관련 설정 이것저것

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import Home from '@/Pages/Home';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false, // API 호출 실패 시 재시도 여부
+      refetchOnMount: true, // 컴포넌트 마운트 시 데이터 요청 여부
+      refetchOnReconnect: true, // 네트워크 재연결 시 데이터 요청 여부
+      refetchOnWindowFocus: true, // 브라우저 포커스 시 데이터 요청 여부
+      staleTime: 1000 * 10, // 10초 동안 데이터 살아있삼
+      throwOnError: true, // 에러 발생 시 예외 던지기 여부
+    },
+  },
+});
+
 function App() {
   return (
-    <div style={{ width: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1rem' }}>
-      <h1>리피카 짱</h1>
-      <a href={`${import.meta.env.VITE_API_URL}/oauth2/authorization/kakao`}>카카오톡 로그인</a>
-      <a href={`${import.meta.env.VITE_API_URL}/oauth2/authorization/google`}>구글 로그인</a>
-      <i>백엔드에몽 로그인 고쳐줘</i>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
   );
 }
 
