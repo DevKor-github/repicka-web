b8a8bd0c8e903d0b2a146e7b49574029fc6c7bab
halionaz <samhsj@korea.ac.kr>
2025-06-24 02:14:59 +0900

    feat: 사용 가능한 색깔들을 토큰화 했삼

--- panda.config.ts
+++ panda.config.ts
@@ -12,7 +12,25 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        colors: {
+          black: { value: '#1A1A1A' },
+          systemGray: { value: '#8E8E93' },
+          systemGray2: { value: '#636366' },
+          systemGray3: { value: '#48484A' },
+          systemGray4: { value: '#3A3A3C' },
+          systemGray5: { value: '#2C2C2E' },
+          systemGray6: { value: '#1C1C1E' },
+          main: { value: '#F83E00' },
+          'main-54': { value: 'rgba(248, 62, 0, 0.54)' },
+          'main-26': { value: 'rgba(248, 62, 0, 0.26)' },
+          '100': { value: 'rgba(255, 255, 255, 1)' },
+          '80': { value: 'rgba(255, 255, 255, 0.80)' },
+          '54': { value: 'rgba(255, 255, 255, 0.54)' },
+        },
+      },
+    },
   },
 
   jsxFramework: 'react',
