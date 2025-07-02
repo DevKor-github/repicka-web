53750a8ef7eef5b5e61e369bf0afbfdd090b553a
halionaz <samhsj@korea.ac.kr>
2025-07-03 03:09:38 +0900

    fix: thx copilot - add strict mode - change navigate method

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import '@/index.css';
 import 'mingcute_icon/font/Mingcute.css';
 
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
 
 import App from '@/App';
-import { BrowserRouter } from 'react-router';
 
 // TODO: BASE_URL 추후 수정
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename={import.meta.env.VITE_BASE_URL || ''}>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter basename={import.meta.env.VITE_BASE_URL || ''}>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 );
--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,14 +1,14 @@
-import SafeArea from '@/common/components/SafeArea';
+import { useNavigate } from 'react-router';
 
 import Chip from '@/common/components/Chip';
-
-// TODO: BASE_URL 추후 수정
-const BASE_URL = import.meta.env.VITE_BASE_URL || '';
+import SafeArea from '@/common/components/SafeArea';
 
 const NotFoundPage = () => {
+  const navigate = useNavigate();
+
   return (
     <SafeArea>
-      <Chip onClick={() => (window.location.href = BASE_URL + '/')}>홈으로 가기</Chip>
+      <Chip onClick={() => navigate('/')}>홈으로 가기</Chip>
       <h1>페이지를 찾을 수 없어요 . . .</h1>
     </SafeArea>
   );
