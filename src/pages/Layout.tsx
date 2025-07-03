19f51b1a8ebc0d5cd432701e111c5f4e7727e5a5
이정원 <jeong-won@ijeong-won-ui-MacBookAir.local>
2025-07-03 13:46:06 +0900

    feat: navigator 메인 탭 네 개에서만 보여주기

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,15 +1,20 @@
 import Navigator from '@/common/components/Navigator';
 import { css } from '@styled-system/css';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 
 /**
  * 모든 페이지들이 공유하는 Layout이에요
  */
 const Layout = () => {
+  const { pathname } = useLocation();
+
+  const visiblePaths = ['/', '/pick', '/chat', '/my'];
+  const showNav = visiblePaths.includes(pathname);
+
   return (
     <div className={css({ position: 'fixed', top: 0, right: 0, bottom: 0, left: 0 })}>
       <Outlet />
-      <Navigator />
+      {showNav && <Navigator />}
     </div>
   );
 };
