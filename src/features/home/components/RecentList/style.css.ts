dd78d864f917e339e8587d61710b91e8d16d19ab
halionaz <samhsj@korea.ac.kr>
2025-07-03 15:31:30 +0900

    feat: 최신 상품 목록 구현

---
+++ src/features/home/components/RecentList/style.css.ts
@@ -0,0 +1,23 @@
+import { css } from '@styled-system/css';
+
+export const Wrapper = css({
+  display: 'flex',
+  flexDir: 'column',
+  gap: '0.625rem',
+  px: '1rem',
+  w: 'full',
+});
+
+export const Title = css({
+  color: '100',
+  fontSize: '1rem',
+  lineHeight: 1.4,
+  letterSpacing: '-0.0255rem',
+  fontWeight: 600,
+});
+
+export const PostList = css({
+  display: 'flex',
+  flexDir: 'column',
+  gap: '1rem',
+});
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,10 +1,21 @@
 import MainTopBar from '@/common/components/MainTopBar';
 import SafeArea from '@/common/components/SafeArea';
+import RecentList from '@/features/home/components/RecentList';
+
+import * as s from './style.css';
+
+import { NAVIGATOR_HEIGHT_PX } from '@/libs/constants/sizes';
 
 const HomePage = () => {
   return (
     <SafeArea>
       <MainTopBar />
+      <div
+        className={s.Container}
+        style={{ height: `calc(100% - ${NAVIGATOR_HEIGHT_PX}px)`, marginBottom: `${NAVIGATOR_HEIGHT_PX}px` }}
+      >
+        <RecentList />
+      </div>
     </SafeArea>
   );
 };
