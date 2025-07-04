9de47c3df0eda342be630a45c3b304b24ecc6e6a
halionaz <samhsj@korea.ac.kr>
2025-07-04 21:23:44 +0900

    feat: TagIcon component

--- src/libs/constants/iconMap.tsx
+++ src/libs/constants/iconMap.tsx
@@ -1,6 +1,4 @@
-import HockeyIcon from '@/libs/assets/HockeyIcon';
 import type { PostType, ProductType, TradeType } from '@/libs/types/post';
-import type { JSX } from 'react';
 
 const iconMap: Record<string, string> = {
   축구: 'mgc_football_fill',
@@ -19,25 +17,20 @@ const iconMap: Record<string, string> = {
   택배거래: 'mgc_package_2_fill',
 };
 
-export const ICON_MAP: Record<ProductType | PostType | TradeType, (className: string) => JSX.Element> = {
-  SOCCER: className => <span className={`mgc_football_fill ${className}`} />,
-  BASKETBALL: className => <span className={`mgc_basketball_fill ${className}`} />,
-  BASEBALL: className => <span className={`mgc_baseball_fill ${className}`} />,
-  HOCKEY: className => (
-    <span className={className}>
-      <HockeyIcon />
-    </span>
-  ),
-  VARSITY_JACKET: className => <span className={`mgc_coat_fill ${className}`} />,
-  SELF_MADE: className => <span className={`mgc_hand_fill ${className}`} />,
-  ACCESSORY: className => <span className={`mgc_scarf_fill ${className}`} />,
-  VINTAGE: className => <span className={`mgc_beard_fill ${className}`} />,
-  REFORM: className => <span className={`mgc_scissors_2_fill ${className}`} />,
-  OTHER: className => <span className={`mgc_coathanger_fill ${className}`} />,
-  RENTAL: className => <span className={`mgc_basket_fill ${className}`} />,
-  SALE: className => <span className={`mgc_wallet_4_fill ${className}`} />,
-  DIRECT: className => <span className={`mgc_group_fill ${className}`} />,
-  DELIVERY: className => <span className={`mgc_package_2_fill ${className}`} />,
-};
-
 export default iconMap;
+
+export const ICON_MAP: Record<Exclude<ProductType, 'HOCKEY'> | PostType | TradeType, string> = {
+  SOCCER: 'mgc_football_fill',
+  BASKETBALL: 'mgc_basketball_fill',
+  BASEBALL: 'mgc_baseball_fill',
+  VARSITY_JACKET: 'mgc_coat_fill',
+  SELF_MADE: 'mgc_hand_fill',
+  ACCESSORY: 'mgc_scarf_fill',
+  VINTAGE: 'mgc_beard_fill',
+  REFORM: 'mgc_scissors_2_fill',
+  OTHER: 'mgc_coathanger_fill',
+  RENTAL: 'mgc_basket_fill',
+  SALE: 'mgc_wallet_4_fill',
+  DIRECT: 'mgc_group_fill',
+  DELIVERY: 'mgc_package_2_fill',
+};
