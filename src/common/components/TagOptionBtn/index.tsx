663c20722d06dfb33429573332e6a1b005b4fa0e
halionaz <samhsj@korea.ac.kr>
2025-07-04 19:55:20 +0900

    fix: lint error & tsc error

--- src/common/components/TagOptionBtn/index.tsx
+++ src/common/components/TagOptionBtn/index.tsx
@@ -6,40 +6,37 @@ import iconMap from '@/common/assets/iconMap';
 import colorMap from '@/common/assets/colorMap';
 
 interface Props extends PropsWithChildren {
-    color?: 'main' | 'gray';
-    variant?: 'icon' | 'color';
-    onClick?: () => void;
+  color?: 'main' | 'gray';
+  variant?: 'icon' | 'color';
+  onClick?: () => void;
 }
 
 const TagOptionBtn = ({ children, color = 'gray', variant = 'icon', onClick }: Props) => {
-    const label = String(children);
-    let leftIconClass = iconMap[label];         // 왼쪽 아이콘 뭐 넣을지
-    let isIcon = !!leftIconClass && variant === 'icon'; // 기타 색상인지 아이콘인지 구분하기 위해서 만듦...
-    let isColor = !!colorMap[label] || variant === 'color';
+  const label = String(children);
+  const leftIconClass = iconMap[label]; // 왼쪽 아이콘 뭐 넣을지
+  const isIcon = !!leftIconClass && variant === 'icon'; // 기타 색상인지 아이콘인지 구분하기 위해서 만듦...
+  const isColor = !!colorMap[label] || variant === 'color';
 
-    const rightIconClass = color === 'main' ? 'mgc_check_fill' : 'mgc_add_fill';
-    
-    const colorValue = colorMap[label];
-    const isGradient = label === '기타'; 
-    const paletteStyle = isGradient       // 동적으로 바꿔줘야 함
-        ? { backgroundImage: 'linear-gradient(180deg, #FF6164 0%, #FF9462 28.85%, #FFBE62 67.31%, #8BFF61 100%)' }
-        : { backgroundColor: colorValue };
+  const rightIconClass = color === 'main' ? 'mgc_check_fill' : 'mgc_add_fill';
 
-    return (
-        <button className={s.Container({ color })} onClick={onClick}>
-            <div className={s.row}>
-                <div className={s.iconLabel}>
-                    {isIcon && (<span className={`${leftIconClass} ${s.leftIcon}`} />)}
-                    {isColor && (<span className={s.colorPalette}
-                            style={paletteStyle}/>)}
-                    {children}
-                </div>
-                <div
-                    className={`${rightIconClass} ${s.rightIcon({ color })}`}
-                />
-            </div>
-        </button>
-    );
+  const colorValue = colorMap[label];
+  const isGradient = label === '기타';
+  const paletteStyle = isGradient // 동적으로 바꿔줘야 함
+    ? { backgroundImage: 'linear-gradient(180deg, #FF6164 0%, #FF9462 28.85%, #FFBE62 67.31%, #8BFF61 100%)' }
+    : { backgroundColor: colorValue };
+
+  return (
+    <button className={s.Container({ color })} onClick={onClick}>
+      <div className={s.row}>
+        <div className={s.iconLabel}>
+          {isIcon && <span className={`${leftIconClass} ${s.leftIcon}`} />}
+          {isColor && <span className={s.colorPalette} style={paletteStyle} />}
+          {children}
+        </div>
+        <div className={`${rightIconClass} ${s.rightIcon({ color })}`} />
+      </div>
+    </button>
+  );
 };
 
 export default TagOptionBtn;
--- src/common/components/TagOptionBtn/style.css.ts
+++ src/common/components/TagOptionBtn/style.css.ts
@@ -1,80 +1,80 @@
-import { cva, css } from "@styled-system/css";
+import { cva, css } from '@styled-system/css';
 
 export const Container = cva({
-    base: {
-        borderRadius: '0.5rem',
-        padding: '0.81rem 0.69rem',
-        display: 'flex',
-        alignItems: 'center',
-        cursor: 'pointer',
-        lineHeight: 'normal',
-        letterSpacing: '-0.04rem',
-        border: '1px solid',
+  base: {
+    borderRadius: '0.5rem',
+    padding: '0.81rem 0.69rem',
+    display: 'flex',
+    alignItems: 'center',
+    cursor: 'pointer',
+    lineHeight: 'normal',
+    letterSpacing: '-0.04rem',
+    border: '1px solid',
+  },
+  variants: {
+    color: {
+      main: {
+        borderColor: 'main-54',
+        bgColor: 'main-26',
+        fontWeight: 600,
+        color: '100',
+      },
+      gray: {
+        borderColor: 'systemGray5',
+        bgColor: 'systemGray5',
+        fontWeight: 400,
+        color: '80',
+      },
     },
-    variants: {
-        color: {
-            main: {
-                borderColor: 'main-54',
-                bgColor: 'main-26',
-                fontWeight: 600,
-                color: '100'
-            },
-            gray: {
-                borderColor: 'systemGray5',
-                bgColor: 'systemGray5',
-                fontWeight: 400,
-                color: '80',
-            }
-        }
-    }
-})
+  },
+});
 // style.css.ts
 export const colorPalette = css({
-    width: '1.25rem',
-    height: '1.25rem',
-    borderRadius: '9999px',
-    padding: '1px',
-    borderColor: 'white',
-    border: '0.125rem solid'
-  });
+  width: '1.25rem',
+  height: '1.25rem',
+  borderRadius: '9999px',
+  padding: '1px',
+  borderColor: 'white',
+  border: '0.125rem solid',
+});
 
 export const row = css({
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    flex: '1'
-})
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  flex: '1',
+});
 
 export const iconLabel = css({
-    display: 'flex',
-    gap: '0.62rem',
-    justifyContent: 'center',
-    alignItems: 'center',
-})
+  display: 'flex',
+  gap: '0.62rem',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
 
 export const leftIcon = css({
-    fontSize: '1.25rem',
-})
+  fontSize: '1.25rem',
+});
 
 export const rightIcon = cva({
-    base: {
-        borderRadius: '0.76rem',
-        width: '1.52225rem',
-        height: '1.52225rem',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
+  base: {
+    borderRadius: '0.76rem',
+    width: '1.52225rem',
+    height: '1.52225rem',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  variants: {
+    color: {
+      main: {
+        color: '80',
+        bgColor: 'main',
+      },
+      gray: {
+        color: '54',
+        bgColor: 'systemGray3',
+      },
     },
-    variants: {
-        color: {
-            main: {
-                color: '80',
-                bgColor: 'main'
-            },
-            gray: {
-                color: '54',
-                bgColor: 'systemGray3',
-            }
-        }
-    }
-})
+  },
+});
