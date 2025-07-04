edad85edfb068c546aeb04154f4eb1330b637abb
halionaz <samhsj@korea.ac.kr>
2025-07-05 02:16:18 +0900

    fix: 헐 이게 되네

--- src/common/components/SafeArea/index.tsx
+++ src/common/components/SafeArea/index.tsx
@@ -6,17 +6,6 @@ import * as s from './style.css';
  * 모바일에서 봤을 때 짤림 영역이 (아마도)없는 AppScreen
  */
 const SafeArea = ({ children }: PropsWithChildren) => {
-  return (
-    <div
-      className={s.Container}
-      style={{
-        top: 'calc(env(safe-area-inset-top))',
-        left: 'calc(env(safe-area-inset-left))',
-        right: 'calc(env(safe-area-inset-right))',
-      }}
-    >
-      {children}
-    </div>
-  );
+  return <div className={s.Container}>{children}</div>;
 };
 export default SafeArea;
