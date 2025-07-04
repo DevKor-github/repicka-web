f697592fcc49880e332d332f20f932e64a42623d
halionaz <samhsj@korea.ac.kr>
2025-07-05 00:24:48 +0900

    feat: 기본적인 Layout & API 연결

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -5,6 +5,7 @@ import Layout from '@/pages/Layout';
 import PickPage from '@/pages/PickPage';
 import PostPageRoutes from './PostPages';
 import NotFoundPage from '@/pages/NotFoundPage';
+import DetailPage from '@/pages/DetailPage';
 
 /**
  * 새로운 페이지 추가하고 싶으면 여기에 추가하면 됩니다
@@ -29,6 +30,10 @@ const routes: RouteObject[] = [
       {
         path: '*', // 못 찾았을 때 404 페이지로 이동
         element: <NotFoundPage />,
+      },
+      {
+        path: '/detail/:id',
+        element: <DetailPage />,
       },
     ],
   },
