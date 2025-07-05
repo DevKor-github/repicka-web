ecbb4b743ce0e991d0afdd02f1224e6161daaa11
이정원 <jeong-won@ijeong-won-ui-MacBookAir.local>
2025-07-05 20:21:22 +0900

    fix: 오타 수정

--- src/features/post/hooks/usePostIem.ts
+++ src/features/post/hooks/usePostIem.ts
@@ -2,7 +2,7 @@
 import { useMutation } from "@tanstack/react-query"     //  보내는 거니까 useMutation
 import { postItem } from "./apis/usePostPost";
 
-export const usePostIem = () => {
+export const usePostItem = () => {
     return useMutation({
         mutationFn: postItem,
     });
