f72ba389a9ab81c01617077a8e8c4676842022e5
halionaz <samhsj@korea.ac.kr>
2025-07-05 17:40:08 +0900

    test: 자꾸 터지는 개복치서버를 대응하기 위해 mock 데이터를 만들어요우

--- src/features/home/hooks/apis/useGetPostList.ts
+++ src/features/home/hooks/apis/useGetPostList.ts
@@ -2,8 +2,42 @@ import client from '@/common/utils/client';
 import type { PostInterface } from '@/libs/types/post';
 import { useQuery } from '@tanstack/react-query';
 
+export interface PostListResponse {
+  message: string;
+  data: PostInterface[];
+}
+
+const MOCK_DATA: PostListResponse = {
+  message: 'success',
+  data: [
+    {
+      id: 1,
+      available: true,
+      chatRoomCount: 5,
+      likeCount: 12,
+      postType: 'RENTAL',
+      price: 10000,
+      title: '빈티지 야구 레플리카(1920년도)',
+      productTypes: ['BASEBALL', 'REFORM'],
+      thumbnail: 'https://crimsonstore.co.kr/web/product/medium/202412/6a5424bbd03c17f28b375a06a31fd4de.jpg',
+    },
+    {
+      id: 2,
+      available: true,
+      chatRoomCount: 0,
+      likeCount: 18,
+      postType: 'RENTAL',
+      price: 100000,
+      title: '빈티지 야구 레플리카(1950년도)',
+      productTypes: ['BASKETBALL', 'REFORM'],
+      thumbnail: 'https://crimsonstore.co.kr/web/product/medium/202412/6a5424bbd03c17f28b375a06a31fd4de.jpg',
+    },
+  ],
+};
+
 const getPostList = async () => {
-  const response = await client.get<{ message: string; data: PostInterface[] }>('/api/v1/post/search');
+  return MOCK_DATA;
+  const response = await client.get<PostListResponse>('/api/v1/post/search');
   return response.data;
 };
 
@@ -11,6 +45,6 @@ export const useGetPostList = () => {
   return useQuery({
     queryKey: ['post-list'],
     queryFn: getPostList,
-    select: data => data.data as PostInterface[],
+    select: data => data.data,
   });
 };
