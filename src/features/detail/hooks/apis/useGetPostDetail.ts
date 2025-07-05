ac941f5e705ec73e5004f8d32621a1e4ed7f4f41
halionaz <samhsj@korea.ac.kr>
2025-07-05 17:40:08 +0900

    test: 자꾸 터지는 개복치서버를 대응하기 위해 mock 데이터를 만들어요우

--- src/features/detail/hooks/apis/useGetPostDetail.ts
+++ src/features/detail/hooks/apis/useGetPostDetail.ts
@@ -27,7 +27,38 @@ export interface PostDetailResponse {
   };
 }
 
+const MOCK_DATA: PostDetailResponse = {
+  message: 'success',
+  data: {
+    id: 1,
+    writer: {
+      id: 1,
+      nickname: '까치는비가와도운다이거최대몇자까지가능한가요',
+      profileImageUrl: 'https://avatars.githubusercontent.com/u/58812281?v=4',
+      isKoreanUnivVerified: true,
+    },
+    itemInfo: {
+      title: '빈티지 야구 레플리카(1920년도)',
+      description:
+        '1920년도 빈티지 야구 레플리카 대여합니다!\n작년에 구매했고, 딱 한 번 입어서 상태 깨끗해요 😀\n안암역 2출 부근에서 직거래 원해요!',
+      canDeal: false,
+      productTypes: ['BASEBALL', 'REFORM'],
+      color: 'CRIMSON',
+      location: '안암역 2출',
+      quality: 'HIGH',
+      tradeMethod: 'DIRECT',
+      size: 'M',
+      saleDate: null,
+    },
+    price: 10000,
+    deposit: 30000,
+    images: ['https://crimsonstore.co.kr/web/product/medium/202412/6a5424bbd03c17f28b375a06a31fd4de.jpg'],
+    postType: 'RENTAL',
+  },
+};
+
 const getPostDetail = async (id: number) => {
+  return MOCK_DATA;
   const response = await client.get<PostDetailResponse>(`/api/v1/post/${id}`);
   return response.data;
 };
