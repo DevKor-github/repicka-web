a04b4d01827c21bd745b544d9251a3406536ee77
이정원 <jeong-won@ijeong-won-ui-MacBookAir.local>
2025-07-06 17:43:59 +0900

    fix: conflict

--- src/features/post/components/WriteLayout/navigator.tsx
+++ src/features/post/components/WriteLayout/navigator.tsx
@@ -1,19 +1,10 @@
 import Btn from '@/common/components/Button';
-<<<<<<< HEAD:src/pages/PostPages/WriteLayout/navigator.tsx
-import StepIndicator from '@/pages/PostPages/StepIndicator';
-import * as s from './style.css';
-
-interface NavigatorProps {
-  totalSteps: number,
-  currentStep: number,
-=======
 import StepIndicator from '@/common/components/StepIndicator';
 import * as s from './style.css';
 
 interface NavigatorProps {
   totalSteps: number;
   currentStep: number;
->>>>>>> 659a5cba7b1128b4903a364677b07e4ec33085b6:src/features/post/components/WriteLayout/navigator.tsx
   goNext: () => void;
   goPrev: () => void;
   isFirst: boolean;
@@ -21,19 +12,11 @@ interface NavigatorProps {
 }
 
 const Navigator = ({ totalSteps, currentStep, goNext, goPrev, isFirst, isLast }: NavigatorProps) => {
-<<<<<<< HEAD:src/pages/PostPages/WriteLayout/navigator.tsx
-
-=======
->>>>>>> 659a5cba7b1128b4903a364677b07e4ec33085b6:src/features/post/components/WriteLayout/navigator.tsx
   const label = isLast ? '완료' : '다음';
 
   return (
     <footer>
-<<<<<<< HEAD:src/pages/PostPages/WriteLayout/navigator.tsx
-      <StepIndicator currentStep={currentStep} totalSteps={totalSteps}/>
-=======
       <StepIndicator currentStep={currentStep} totalSteps={totalSteps} />
->>>>>>> 659a5cba7b1128b4903a364677b07e4ec33085b6:src/features/post/components/WriteLayout/navigator.tsx
       <div className={s.stepBtn}>
         {!isFirst && (
           <div className={s.halfFlex}>
