7b58a5b28951e2371bdcb3fead7b1fb6c187f75a
halionaz <samhsj@korea.ac.kr>
2025-07-06 22:11:58 +0900

    fix(design): 인디케이터에 그림자 추가!

--- src/features/detail/components/StepIndicator/style.css.ts
+++ src/features/detail/components/StepIndicator/style.css.ts
@@ -11,6 +11,7 @@ export const progressCircle = cva({
     height: '0.375rem',
     transition: 'all 0.3s ease-in-out',
     rounded: 'full',
+    boxShadow: '0px 0px 4px 0px rgba(0, 0, 0, 0.60)',
   },
   variants: {
     isCurrent: {
