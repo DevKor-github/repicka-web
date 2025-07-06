9a70282e207d93aa60bfb23fe679a661f5d80ac8
jeongwon <123575547+jw0202058@users.noreply.github.com>
2025-07-06 15:57:47 +0900

    Update src/features/post/hooks/apis/usePostPost.ts
    Co-authored-by: halion <samhsj@korea.ac.kr>

--- src/features/post/hooks/apis/usePostPost.ts
+++ src/features/post/hooks/apis/usePostPost.ts
@@ -4,7 +4,7 @@ import type { PostPayload } from '@/features/post/types/post';
 export const postItem = async (payload: PostPayload) => {
   const token = localStorage.getItem('ACCESS_TOKEN');
 
-  const response = await client.post(`${import.meta.env.BASE_URL}}/api/v1/post`, payload, {
+  const response = await client.post('/api/v1/post', payload, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
