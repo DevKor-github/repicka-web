fd08b857f7d7c59301ae75169ef523fb7e86dc02
이정원 <jeong-won@ijeong-won-ui-MacBookAir.local>
2025-07-06 20:55:24 +0900

    feat: step1~6 레이아웃 구현

--- src/features/post/components/StepFunnel/style.css.ts
+++ src/features/post/components/StepFunnel/style.css.ts
@@ -18,7 +18,6 @@ export const Container = css({
 export const Grid = css({
   display: 'grid',
   gridTemplateColumns: 'repeat(2, 1fr)',
-  // padding: '1rem 0 0 0',
   gap: '0.62rem',
 });
 
@@ -35,5 +34,72 @@ export const Head = css({
 });
 
 export const Content = css({
+  display: 'flex',
+  flexDirection: 'column',
   margin: '2.97rem 0 0 0',
+  gap: '2.25rem'
 });
+
+export const DetailContent = css({
+  display: 'flex',
+  flexDirection: 'column',
+  color: '100',
+  fontFamily: 'Pretendard',
+  fontSize: '1rem',
+  fontStyle: 'normal',
+  fontWeight: '500',
+  lineHeight: '1.4',
+  letterSpacing: '-0.04rem',
+  gap: '0.875rem'
+})
+
+export const ContentHeader = css({
+  display: 'flex',
+  alignItems: 'center',
+  alignSelf: 'stretch',
+  gap: '0.38rem'
+})
+
+export const ChipColumn = css({
+  display: 'flex',
+  alignItems: 'flex-start',
+  alignContent: 'flex-start',
+  gap: '0.5625rem 0.5rem',
+  alignSelf: 'stretch',
+  flexWrap: 'wrap',
+})
+
+export const TokenContainer = css({
+  marginBottom: '0.75rem'
+})
+
+export const HeaderInputField = css({   // step6에서 
+  display: 'flex',
+  flexDirection: 'column',
+  margin: '0 0 1.25rem 0',
+  gap: '1rem'
+})
+
+export const PriceContent = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  alignSelf: 'stretch',
+  color: '100',
+  fontFamily: 'Pretendard',
+  fontSize: '1rem',
+  fontStyle: 'normal',
+  fontWeight: '500',
+  lineHeight: '1.4',
+  letterSpacing: '-0.04rem'
+})
+
+export const CanDeal = css({
+  color: '80',
+  fontFamily: 'Pretendard',
+  fontSize: '0.875rem',
+  fontStyle: 'normal',
+  fontWeight: '500',
+  lineHeight: '1.4',
+  letterSpacing: '-0.035rem'
+})
