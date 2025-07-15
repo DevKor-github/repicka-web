a8beedbf7c955d634f928a3abc93a036a62690a6
jw0202058 <jw0202058@korea.ac.kr>
2025-07-15 19:28:30 +0900

    feat: step3 경우의 수 추가

--- src/features/post/components/StepFunnel/Step3/index.tsx
+++ src/features/post/components/StepFunnel/Step3/index.tsx
@@ -4,6 +4,7 @@ import { TRADE_METHODS_MAP, type TradeMethods } from '@/libs/types/item';
 import Token from '@/common/components/Token';
 import InputField from '../../InputField';
 import { useStep3Store } from '@/features/post/stores/Step3Store';
+import { useStep1Store } from '@/features/post/stores/Step1Store';
 
 const Location = () => {
 
@@ -20,11 +21,28 @@ const Location = () => {
   );
 }
 
+const Address = () => {
+  // TODO: 수정하기,,,
+  // const addrStore = '';
+  // const addrSetter = () => void;
+
+  return (
+    <div className={c.DetailContent}>
+      <div className={c.DetailContent}>
+        <span>대여해준 상품을 돌려받을 장소를 입력해 주세요</span>
+        {/* <InputField value={addrStore} setValue={addrSetter} /> */}
+      </div>
+    </div>
+  );
+}
+
 const Step3 = () => {
   const tradeMethodStore = useStep3Store(state => state.tradeMethods);
   const tradeMethodSetter = useStep3Store(state => state.setTradeMethods);
 
   const isDirect = tradeMethodStore.includes('DIRECT')
+  const isParcel = tradeMethodStore.includes('PARCEL')
+  const isRental = useStep1Store(state => state.transactionTypes).includes('RENTAL');
 
   const handleSelectType = (type: TradeMethods) => {
     const updated = tradeMethodStore.includes(type)
@@ -52,6 +70,7 @@ const Step3 = () => {
           </div>
         </div>
         {isDirect && <Location />}
+        {isRental && isParcel && <Address />}
       </div>
     </div>
   );
