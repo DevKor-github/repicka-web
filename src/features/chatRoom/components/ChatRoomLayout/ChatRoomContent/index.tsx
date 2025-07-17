6ae8313c901fb482312c05bcc2f6311a7e0ad2be
jw0202058 <jw0202058@korea.ac.kr>
2025-07-17 17:12:27 +0900

    chore: 더미 데이터 추가

--- src/features/chatRoom/components/ChatRoomLayout/ChatRoomContent/index.tsx
+++ src/features/chatRoom/components/ChatRoomLayout/ChatRoomContent/index.tsx
@@ -2,27 +2,44 @@ import MyChat from '../../MyChat';
 import OtherChat from '../../OtherChat';
 import * as s from './style.css';
 
-export const ChatRoomContent = () => {
-  // [ TODO ]
-  // 스크롤 맨 아래가 디폴트
-  // 스크롤 좀 올렸을 때 맨 아래로 가는 버튼도 만들어야 하려나
-  // 키보드 올리면 화면 밀려 올라가게
-  // 내 채팅, 상대 채팅 묶어서 gap 서로 다르게
-  // 날짜 그룹핑
-  // api 붙이기~~~ camera, send 아이콘 동작 붙이기~~~
-  // 픽 확인 컴포넌트 만들기~~~
+// [ TODO ]
+// 스크롤 맨 아래가 디폴트
+// 스크롤 좀 올렸을 때 맨 아래로 가는 버튼도 만들어야 하려나
+// 키보드 올리면 화면 밀려 올라가게
+// isMine, 날짜 그룹핑
+// api 붙이기~~~ camera, send 아이콘 동작 붙이기~~~
+// 픽 확인 컴포넌트 만들기~~~
 
+interface Chat {
+  isMine: boolean;
+  message: string;
+  time: string;
+}
+
+const dummyData: Chat[] = [
+  { isMine: true, message: '응 어쩔', time: '오후 6:32' },
+  { isMine: true, message: '배고파', time: '오후 6:32' },
+  { isMine: true, message: '응', time: '오후 6:33' },
+  { isMine: false, message: '으아아아아ㅏ 노페인 노개인 음악업슨 ㄴ세상', time: '오후 6:34' },
+  { isMine: false, message: '요즘 매미가 비가 와도 안 울어', time: '오후 6:34' },
+  { isMine: false, message: '어떤 데이터가 올까...?', time: '오후 6:34' },
+  { isMine: true, message: 'isMine도 보내주나? 아님 내가 체크해야 하나?', time: '오후 6:33' },
+];
+
+export const ChatRoomContent = () => {
   return (
     <div className={s.Wrapper}>
-      <MyChat time="오후 6:32">응 어쩔</MyChat>
-      <OtherChat time="오후 6:32">흠ㅁ...</OtherChat>
-      <OtherChat time="오후 6:32">노페인 노개인 음악없ㅂ는 세상 ㅇㅇㅡ아아아</OtherChat>
-      <MyChat time="오후 6:32">소리 지르지 마세요 소리지르는니가 챔피언</MyChat>
-      <OtherChat time="오후 6:32">스크롤이 잘되는지 확인해보기</OtherChat>
-      <MyChat time="오후 6:32">으악 채팅 할 거 많다</MyChat>
-      <MyChat time="오후 6:32">일단 디자인이랑 같은지, 뭐가 더 필요한지 체크하기...</MyChat>
-      <MyChat time="오후 6:32">망고빙수 먹고 싶당</MyChat>
-      <OtherChat time="오후 6:32">스크롤 체크</OtherChat>
+      {dummyData.map((chat, index) =>
+        chat.isMine ? (
+          <MyChat key={index} time={chat.time}>
+            {chat.message}
+          </MyChat>
+        ) : (
+          <OtherChat key={index} time={chat.time}>
+            {chat.message}
+          </OtherChat>
+        ),
+      )}
     </div>
   );
 };
