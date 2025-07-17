350f45225288f0678bb08b41386bad95ff4b18ff
jw0202058 <jw0202058@korea.ac.kr>
2025-07-17 21:05:09 +0900

    feat: date 파싱

--- src/features/chatRoom/components/ChatRoomLayout/ChatRoomContent/index.tsx
+++ src/features/chatRoom/components/ChatRoomLayout/ChatRoomContent/index.tsx
@@ -1,3 +1,4 @@
+import { parseTime } from '@/common/utils/parseDate';
 import MyChat from '../../MyChat';
 import OtherChat from '../../OtherChat';
 import * as s from './style.css';
@@ -13,39 +14,41 @@ import * as s from './style.css';
 interface Chat {
   isMine: boolean;
   message: string;
-  time: string;
+  date: string;
 }
 
 const dummyData: Chat[] = [
-  { isMine: true, message: '응 어쩔', time: '오후 6:32' },
-  { isMine: true, message: '배고파', time: '오후 6:32' },
-  { isMine: true, message: '응', time: '오후 6:33' },
-  { isMine: false, message: '으아아아아ㅏ 노페인 노개인 음악업슨 ㄴ세상', time: '오후 6:34' },
-  { isMine: false, message: '요즘 매미가 비가 와도 안 울어', time: '오후 6:34' },
-  { isMine: false, message: '어떤 데이터가 올까...?', time: '오후 6:34' },
-  { isMine: true, message: 'isMine도 보내주나? 아님 내가 체크해야 하나?', time: '오후 6:33' },
-  { isMine: true, message: '스크롤 체크를 해보아요', time: '오후 6:33' },
-  { isMine: true, message: '그룹핑이 바로 전 거랑 내가 직접 비교하는 수밖에 없나', time: '오후 6:33' },
-  { isMine: false, message: '나 내일 권예진이랑 초밥 먹는당', time: '오후 6:34' },
-  { isMine: false, message: '부럽지 ㅋㅋ', time: '오후 6:34' },
-  { isMine: true, message: '너무 부럽다.', time: '오후 6:33' },
-  { isMine: true, message: '집 가야지 하하하 예재무 공부해야지', time: '오후 6:33' },
+  { isMine: true, message: '응 어쩔', date: '2025-07-09T15:56:31.591185' },
+  { isMine: true, message: '배고파', date: '2025-07-09T15:56:31.591185' },
+  { isMine: true, message: '응', date: '2025-07-09T15:57:31.591185' },
+  { isMine: false, message: '으아아아아ㅏ 노페인 노개인 음악업슨 ㄴ세상', date: '2025-07-09T15:57:31.591185' },
+  { isMine: false, message: '요즘 매미가 비가 와도 안 울어', date: '2025-07-09T15:58:31.591185' },
+  { isMine: false, message: '어떤 데이터가 올까...?', date: '2025-07-09T15:58:31.591185' },
+  { isMine: true, message: 'isMine도 보내주나? 아님 내가 체크해야 하나?', date: '2025-07-09T15:59:31.591185' },
+  { isMine: true, message: '스크롤 체크를 해보아요', date: '2025-07-09T15:59:31.591185' },
+  { isMine: true, message: '그룹핑이 바로 전 거랑 내가 직접 비교하는 수밖에 없나', date: '2025-07-09T15:59:31.591185' },
+  { isMine: false, message: '나 내일 권예진이랑 초밥 먹는당', date: '2025-07-09T15:59:31.591185' },
+  { isMine: false, message: '부럽지 ㅋㅋ', date: '2025-07-09T16:00:31.591185' },
+  { isMine: true, message: '너무 부럽다.', date: '2025-07-09T16:00:31.591185' },
+  { isMine: true, message: '집 가야지 하하하 예재무 공부해야지', date: '2025-07-09T16:01:31.591185' },
 ];
 
 export const ChatRoomContent = () => {
   return (
     <div className={s.Wrapper}>
-      {dummyData.map((chat, index) =>
-        chat.isMine ? (
-          <MyChat key={index} time={chat.time}>
+      {dummyData.map((chat, index) => {
+        const time = parseTime(chat.date);
+
+        return chat.isMine ? (
+          <MyChat key={index} time={time}>
             {chat.message}
           </MyChat>
         ) : (
-          <OtherChat key={index} time={chat.time}>
+          <OtherChat key={index} time={time}>
             {chat.message}
           </OtherChat>
-        ),
-      )}
+        );
+      })}
     </div>
   );
 };
