65b554cf86cdd133a8b0a8c9436d01f04f2327e8
jw0202058 <jw0202058@korea.ac.kr>
2025-07-17 21:33:25 +0900

    chore: todo 주석 달기

--- src/features/chatRoom/components/PickChat/index.tsx
+++ src/features/chatRoom/components/PickChat/index.tsx
@@ -2,11 +2,13 @@ import { Link } from 'react-router';
 import * as s from './style.css';
 
 export const PickChat = () => {
+  // TODO : 이름 넣기, pick으로 연결하기
+
   return (
     <div className={s.Container}>
       <div className={s.HeadText}>민채 님께서 설정하신 대여 정보가 도착했어요</div>
       <div className={s.Image} />
-      <Link className={s.PickBtn} to={'api/pick'}>
+      <Link className={s.PickBtn} to={'/pick'}>
         <div className={s.PickText}>PICK 확인하기</div>
       </Link>
     </div>
