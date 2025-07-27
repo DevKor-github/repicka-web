4c95d9cbb1fd60de42a19b3fb176d195645abc07
halionaz <samhsj@korea.ac.kr>
2025-07-27 17:10:05 +0900

    fix: no vite image prefix case

--- src/common/utils/getImageUrl.ts
+++ src/common/utils/getImageUrl.ts
@@ -1,11 +1,10 @@
+const prefixUrl = import.meta.env.VITE_IMAGE_PREFIX_URL || '';
+
 const getImageUrl = (imageUrl: string) => {
   if (imageUrl.startsWith('http')) {
     return imageUrl;
   }
-  const prefixUrl = import.meta.env.VITE_IMAGE_PREFIX_URL || '';
-  if (!prefixUrl) {
-    throw new Error('VITE_IMAGE_PREFIX_URL is not defined or is empty.');
-  }
+
   return `${prefixUrl}/${imageUrl}`;
 };
 
