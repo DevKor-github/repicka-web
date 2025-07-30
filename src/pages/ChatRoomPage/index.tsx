f8f102a9e14db70312d81aa9a52a785c965dc4ea
jw0202058 <jw0202058@korea.ac.kr>
2025-07-31 01:59:03 +0900

    chore: 주석 정리

--- src/pages/ChatRoomPage/index.tsx
+++ src/pages/ChatRoomPage/index.tsx
@@ -1,7 +1,3 @@
-// 이 페이지로 들어오는 link 부모 컴포넌트에서 chatRoomId를 주니까,
-// 거기에서 받아서 이 페이지로 들어오면서 post 요청 쏘기
-// 그리고 chatRoomContent로 res.data 전달해 줘야 함
-
 import ChatRoomContent from '@/features/chatRoom/components/ChatRoomLayout/ChatRoomContent';
 import ChatRoomFooter from '@/features/chatRoom/components/ChatRoomLayout/ChatRoomFooter';
 import ChatRoomHeader from '@/features/chatRoom/components/ChatRoomLayout/ChatRoomHeader';
