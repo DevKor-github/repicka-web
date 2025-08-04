50e7ad29fc9546ab646192d2b16812b8bb24bd8a
halionaz <samhsj@korea.ac.kr>
2025-08-04 23:30:32 +0900

    fix: spinner 애니메이션

--- src/common/components/Spinner/style.css.ts
+++ src/common/components/Spinner/style.css.ts
@@ -2,12 +2,13 @@ import { css } from '@styled-system/css';
 
 // TODO: 스타일 변경
 export const SpinnerContainer = css({
-  display: 'flex',
-  alignSelf: 'center',
   width: '40px',
   height: '40px',
+
+  display: 'flex',
+  borderRadius: '50%',
+  alignSelf: 'center',
   border: '4px solid #f3f3f3',
   borderTop: '4px solid {colors.main}',
-  borderRadius: '50%',
   animation: 'spin 0.5s linear infinite',
 });
