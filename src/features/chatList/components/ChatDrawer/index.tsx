5120e2d3ef33826ec80bde349be2bc79151e4854
jw0202058 <jw0202058@korea.ac.kr>
2025-08-17 14:26:57 +0900

    fix: refactor

--- src/features/chatList/components/ChatDrawer/index.tsx
+++ src/features/chatList/components/ChatDrawer/index.tsx
@@ -1,27 +1,33 @@
 import { cx } from '@styled-system/css';
 import * as s from './style.css';
 
-interface Props {
+interface DrawerProps {
   onLeave: () => void;
   onReport: () => void;
   onRead: () => void;
 }
 
-const ChatDrawer = ({ onLeave, onRead, onReport }: Props) => {
+interface BtnProps {
+  onClick: () => void;
+  icon: string;
+  title: string;
+}
+
+const Button = ({ onClick, icon, title }: BtnProps) => {
+  return (
+    <button className={s.Button} onClick={onClick}>
+      <div className={cx(icon, s.Icon)} />
+      {title}
+    </button>
+  );
+};
+
+const ChatDrawer = ({ onLeave, onRead, onReport }: DrawerProps) => {
   return (
     <div className={s.ButtonWrapper}>
-      <button className={s.Button} onClick={onRead}>
-        <div className={cx('mgc_task_fill', s.Icon)} />
-        <div>읽음으로 표시하기</div>
-      </button>
-      <button className={s.Button} onClick={onLeave}>
-        <div className={cx('mgc_entrance_fill', s.Icon, s.Reverse)} />
-        <div>채팅방 퇴장하기</div>
-      </button>
-      <button className={s.Button} onClick={onReport}>
-        <div className={cx('mgc_warning_fill', s.Icon)} />
-        <div>신고하기</div>
-      </button>
+      <Button icon="mgc_task_fill" onClick={onRead} title="읽음으로 표시하기" />
+      <Button icon="mgc_exit_fill" onClick={onLeave} title="채팅방 퇴장하기" />
+      <Button icon="mgc_warning_fill" onClick={onReport} title="신고하기" />
     </div>
   );
 };
