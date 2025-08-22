4a09d5b49fc820b3cd4df78f49151feab474c7ee
halionaz <samhsj@korea.ac.kr>
2025-08-22 17:54:37 +0900

    feat: 날짜 선택 버튼 글자 바뀜

--- src/features/home/components/DatePickButton/index.tsx
+++ src/features/home/components/DatePickButton/index.tsx
@@ -3,18 +3,25 @@ import * as s from './style.css';
 import useDrawer from '@/common/hooks/useDrawer';
 import Drawer from '@/common/components/Drawer';
 import DateFilter from '@/features/home/components/DateFilter';
+import { useSearchParams } from 'react-router';
+import { formatDate } from 'date-fns';
 
 interface Props {
   itemCounts: number;
 }
 const DatePickButton = ({ itemCounts }: Props) => {
+  const [searchParams] = useSearchParams();
   const { open, close, drawerState } = useDrawer();
+
+  const startDate = searchParams.get('start-date') ? new Date(searchParams.get('start-date') as string) : null;
+  const endDate = searchParams.get('end-date') ? new Date(searchParams.get('end-date') as string) : null;
+  const isSelected = startDate !== null && endDate !== null;
 
   return (
     <>
-      <button className={s.Container} onClick={open}>
+      <button className={s.Container({ isSelected })} onClick={open}>
         <span className={cx('mgc_calendar_fill', s.Icon)} />
-        날짜선택
+        {isSelected ? `${formatDate(startDate, 'MM.dd')} ~ ${formatDate(endDate, 'MM.dd')}` : '날짜선택'}
       </button>
       <Drawer title="날짜" description="대여 및 반납일자를 선택해 주세요" drawerState={drawerState}>
         <DateFilter itemCounts={itemCounts} close={close} />
--- src/features/home/components/DatePickButton/style.css.ts
+++ src/features/home/components/DatePickButton/style.css.ts
@@ -1,16 +1,27 @@
-import { css } from '@styled-system/css';
+import { css, cva } from '@styled-system/css';
 
-export const Container = css({
-  display: 'flex',
-  p: '0.25rem 0.625rem 0.25rem 0.5rem',
-  alignItems: 'center',
-  gap: '0.1875rem',
-  rounded: '0.625rem',
-  bg: 'systemGray5',
-  color: '100',
-  fontSize: '0.75rem',
-  fontWeight: 500,
-  lineHeight: '0.87788rem',
+export const Container = cva({
+  base: {
+    display: 'flex',
+    p: '0.25rem 0.625rem 0.25rem 0.5rem',
+    alignItems: 'center',
+    gap: '0.1875rem',
+    rounded: '0.625rem',
+    color: '100',
+    fontSize: '0.75rem',
+    fontWeight: 500,
+    lineHeight: '0.87788rem',
+  },
+  variants: {
+    isSelected: {
+      true: {
+        bgColor: 'main-26',
+      },
+      false: {
+        bgColor: 'systemGray5',
+      },
+    },
+  },
 });
 
 export const Icon = css({
