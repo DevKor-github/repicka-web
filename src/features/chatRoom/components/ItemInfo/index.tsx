51e6e19386b630f0b3917593b8d561fc229fdbbf
jw0202058 <jw0202058@korea.ac.kr>
2025-08-25 02:21:22 +0900

    fix: 채팅방 내 itemInfo 타입 수정

--- src/features/chatRoom/components/ItemInfo/index.tsx
+++ src/features/chatRoom/components/ItemInfo/index.tsx
@@ -17,11 +17,11 @@ const ItemInfo = ({ data }: Props) => {
       <div className={s.PriceContainer}>
         <div className={s.Price}>
           <span>대여료</span>
-          <p>{data.rentalFee}원</p>
+          <p>{data.rentalFee.toLocaleString()}원</p>
         </div>
         <div className={s.Price}>
           <span>보증금</span>
-          <p>{data.deposit}원</p>
+          <p>{data.deposit.toLocaleString()}원</p>
         </div>
       </div>
     );
@@ -31,7 +31,7 @@ const ItemInfo = ({ data }: Props) => {
     return (
       <div className={s.Price}>
         <span>판매가</span>
-        <p>{data.salePrice}원</p>
+        <p>{data.salePrice.toLocaleString()}원</p>
       </div>
     );
   };
@@ -41,8 +41,8 @@ const ItemInfo = ({ data }: Props) => {
       <img className={s.Thumbnail} src={getImageUrl(data.thumbnail)} />
       <div className={s.Info}>
         <span>{data.title}</span>
-        {isRental && <Sale />}
-        {isSale && <Rental />}
+        {isSale && <Sale />}
+        {isRental && <Rental />}
       </div>
       <div className={cx('mgc_right_line', s.Icon)} />
     </Link>
