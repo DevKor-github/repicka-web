b3eede1455e74a2c48563a520c28096f9341b13b
halionaz <samhsj@korea.ac.kr>
2025-08-26 03:26:50 +0900

    fix: content type

--- src/common/apis/usePatchFCMToken.ts
+++ src/common/apis/usePatchFCMToken.ts
@@ -3,7 +3,11 @@ import { useMutation } from '@tanstack/react-query';
 import client from '@/common/utils/client';
 
 const patchFCMToken = async (token: string) => {
-  const response = await client.patch('/api/v1/user/fcm-token', token);
+  const response = await client.patch('/api/v1/user/fcm-token', token, {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
   return response.data;
 };
 
--- src/common/apis/useTestFCMToken.ts
+++ src/common/apis/useTestFCMToken.ts
@@ -2,7 +2,11 @@ import client from '@/common/utils/client';
 import { useMutation } from '@tanstack/react-query';
 
 const testFCMToken = async (token: string) => {
-  const response = await client.post('/api/test/fcm', token);
+  const response = await client.post('/api/test/fcm', token, {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
   return response.data;
 };
 
