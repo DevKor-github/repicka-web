7eda977e032d8f21a2a4830c0cbfea02c700cdf9
halionaz <samhsj@korea.ac.kr>
2025-09-02 14:03:35 +0900

    feat: add external banner img - dekode

--- src/features/home/components/Banner/index.tsx
+++ src/features/home/components/Banner/index.tsx
@@ -1,33 +1,42 @@
 import { Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+import banner1 from '@/libs/assets/external_banner_1.webp';
+import banner2 from '@/libs/assets/external_banner_2.webp';
+
 import * as s from './style.css';
 import { useState } from 'react';
 
+export interface BannerInterface {
+  title?: string;
+  description?: string;
+  author?: string;
+  src: string;
+  link?: string;
+}
 // TODO: 실제 API로 교체
-const MOCK_DATA = [
-  {
-    title: '아 배고프다 육회 먹고 싶다',
-    description: '간장 양념 고추장 양념',
-    author: '까치',
-    src: 'https://github.com/user-attachments/assets/66bed657-d8a9-464a-9d9a-f9647028c1d7',
-  },
-  {
-    title: '토리텐 붓카케 우동',
-    description: '새우튀김 추가',
-    author: '호랑이',
-    src: 'https://www.skyweaver.net/images/media/wallpapers/wallpaper1.jpg',
-  },
+const BANNER_DATA: BannerInterface[] = [
   {
     title: '어떻게 삼시세끼 불닭',
     description: '오늘 학식은 뭔가요',
     author: '이따다끼마스',
     src: 'https://t3.ftcdn.net/jpg/05/64/82/08/360_F_564820811_n9WP1mM43pLiQwLkIA07KF9Hat5vkX2v.jpg',
   },
+  {
+    title: '디코드',
+    description: '삑 그리고 담 야 삑 그리고 담야',
+    author: '빈췐',
+    src: banner1,
+    link: 'https://linktr.ee/eugssmixx',
+  },
+  {
+    src: banner2,
+    link: 'https://linktr.ee/eugssmixx',
+  },
 ];
 
 const Banner = () => {
-  const [data] = useState(MOCK_DATA);
+  const [data] = useState(BANNER_DATA);
   const totalIndex = data.length;
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -47,7 +56,9 @@ const Banner = () => {
       >
         {data.map((item, index) => (
           <SwiperSlide key={`${item.title}-${index}`}>
-            <img className={s.Image} src={item.src} alt={item.title} />
+            <a href={item.link} target="_blank" rel="noopener noreferrer">
+              <img className={s.Image} src={item.src} alt={item.title} />
+            </a>
           </SwiperSlide>
         ))}
       </Swiper>
@@ -57,8 +68,12 @@ const Banner = () => {
           <div className={s.Title}>{currentData.title}</div>
           <div className={s.DescriptionWrapper}>
             <span>{currentData.description}</span>
-            <span className={s.Dot} />
-            <span>{currentData.author}</span>
+            {currentData.author && (
+              <>
+                <span className={s.Dot} />
+                <span>{currentData.author}</span>
+              </>
+            )}
           </div>
         </div>
         <div className={s.Fraction}>
