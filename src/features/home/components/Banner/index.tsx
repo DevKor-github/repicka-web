bdfcbc410d6ecb0e6fcd7aadd2b05c954015c398
halionaz <samhsj@korea.ac.kr>
2025-09-04 13:08:14 +0900

    feat: 배너 적용

--- src/features/home/components/Banner/index.tsx
+++ src/features/home/components/Banner/index.tsx
@@ -1,39 +1,9 @@
 import { Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import banner1 from '@/libs/assets/external_banner_1.webp';
-import banner2 from '@/libs/assets/external_banner_2.webp';
-
 import * as s from './style.css';
 import { useState } from 'react';
-
-export interface BannerInterface {
-  title?: string;
-  description?: string;
-  author?: string;
-  src: string;
-  link?: string;
-}
-// TODO: 실제 API로 교체
-const BANNER_DATA: BannerInterface[] = [
-  {
-    title: '어떻게 삼시세끼 불닭',
-    description: '오늘 학식은 뭔가요',
-    author: '이따다끼마스',
-    src: 'https://t3.ftcdn.net/jpg/05/64/82/08/360_F_564820811_n9WP1mM43pLiQwLkIA07KF9Hat5vkX2v.jpg',
-  },
-  {
-    title: '디코드',
-    description: '삑 그리고 담 야 삑 그리고 담야',
-    author: '빈췐',
-    src: banner1,
-    link: 'https://linktr.ee/eugssmixx',
-  },
-  {
-    src: banner2,
-    link: 'https://linktr.ee/eugssmixx',
-  },
-];
+import { BANNER_DATA } from '@/features/home/constants';
 
 const Banner = () => {
   const [data] = useState(BANNER_DATA);
---
+++ src/features/home/constants.ts
@@ -0,0 +1,29 @@
+import main_banner from '@/libs/assets/banner.webp';
+import banner1 from '@/libs/assets/external_banner_1.webp';
+import banner2 from '@/libs/assets/external_banner_2.webp';
+
+export interface BannerInterface {
+  title?: string;
+  description?: string;
+  author?: string;
+  src: string;
+  link?: string;
+}
+
+// TODO: 실제 API로 교체
+export const BANNER_DATA: BannerInterface[] = [
+  {
+    title: 'de:Kode your Color',
+    description: '고연전 레플리카도 디코드와 함께!',
+    author: 'de:Kode',
+    src: banner1,
+    link: 'https://linktr.ee/eugssmixx',
+  },
+  {
+    title: 'de:Kode your Color',
+    description: '고연전 레플리카도 디코드와 함께!',
+    author: 'de:Kode',
+    src: banner2,
+    link: 'https://linktr.ee/eugssmixx',
+  },
+];
