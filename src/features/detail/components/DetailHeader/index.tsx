6d1fa129217d57a16df6376743553eed991238ec
halionaz <samhsj@korea.ac.kr>
2025-09-05 13:18:21 +0900

    feat: 게시글 공유 기능

--- src/features/detail/components/DetailHeader/index.tsx
+++ src/features/detail/components/DetailHeader/index.tsx
@@ -5,6 +5,7 @@ import * as s from './style.css';
 import useDrawer from '@/common/hooks/useDrawer';
 import OptionDrawer from '@/features/detail/components/OptionDrawer';
 import type { ItemDetailInterface } from '../../types';
+import { useToast } from '@/common/hooks/useToast';
 
 interface Props {
   state: ItemDetailInterface;
@@ -12,8 +13,13 @@ interface Props {
 const DetailHeader = ({ state }: Props) => {
   const navigate = useNavigate();
   const { open, drawerState } = useDrawer();
+  const { openToast } = useToast();
 
   const goBack = () => navigate(-1);
+  const handleShare = () => {
+    navigator.clipboard.writeText(window.location.href);
+    openToast({ message: '링크가 복사되었어요' });
+  };
 
   return (
     <>
@@ -21,7 +27,7 @@ const DetailHeader = ({ state }: Props) => {
       <header className={s.Container}>
         <button className={`mgc_left_line ${s.BackButton}`} onClick={goBack} aria-label="Go back" />
         <div className={s.RightSide}>
-          <button className={`${s.RightButton} mgc_share_3_fill`} aria-label="Share item" />
+          <button className={`${s.RightButton} mgc_share_3_fill`} aria-label="Share item" onClick={handleShare} />
           <button className={`${s.RightButton} mgc_more_2_fill`} onClick={open} />
         </div>
       </header>
